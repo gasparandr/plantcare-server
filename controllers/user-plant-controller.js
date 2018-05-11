453745fc2e142de61a4ffb06900329fd6dca4996
Andrei Gaspar <andrei@ildiesign.com>
2018-05-12 02:24:50 +0300

    - Water single plant

--- controllers/user-plant-controller.js
+++ controllers/user-plant-controller.js
@@ -1,11 +1,24 @@
 const User = require("../models/user");
 const UserPlantGroup = require("../models/user-plant-group");
+const UserPlant = require("../models/user-plant");
 
 
 module.exports = {
 
     water(req, res, next) {
+        const plantId = req.params.id;
 
+        UserPlant.findById(plantId)
+            .then( (plant) => {
+                const today = new Date();
+
+                plant.lastWatered = today;
+                plant.wateringHistory.push( today );
+
+                return plant.save();
+            })
+            .then( res.send({ success: true, message: "Plant successfully watered." }))
+            .catch( next );
     }
 
 };
