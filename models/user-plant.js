ebbbb92da1dbdeeed15a4bf5c2df6b5844dcfff9
Andrei Gaspar <andrei@ildiesign.com>
2018-05-11 22:18:12 +0300

    - Minimal user plant model

---
+++ models/user-plant.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const UserPlantSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    description: String,
+
+    source: {
+        type: Schema.Types.ObjectId,
+        ref: "Plant"
+    }
+
+
+});
